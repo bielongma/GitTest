b7a8098c17e3979df32c724530a1b8c122c2ce1a
Lhjun <Lhjun@atguigu.com>
2021-10-28 15:04:54 +0800

    push test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("test3");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
